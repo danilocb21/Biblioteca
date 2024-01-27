e1025bbb8cb7ff3743d7c7c142028452f93cc3cd
Gabriel Campelo <gabriel.campfor@gmail.com>
2024-01-27 20:52:23 -0300

    Update Sorting

---
+++ Codigo/Sorting/BubbleSort.cpp
@@ -0,0 +1,35 @@
+// comparações: C(n) = O(n^2)
+// trocas: T(n) = O(n^2)
+// adptativo: O(n) quando vetor está parcialmente ordenado
+
+// Vantagens:
+// algoritmo estável 
+// simples
+
+// Desvantagens:
+// não adaptável
+// muitas trocas
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void bubbleSort(int *ar, int n){
+    int i, aux;
+    bool troca;
+
+    do {
+        troca = false;
+        for(int i = 0; i < n - 1; i++){
+            if(ar[i + 1] < ar[i]){
+                aux = ar[i + 1];
+                ar[i + 1] = ar[i];
+                ar[i] = aux;
+                troca = true;
+            }
+        }
+
+    } while(troca);
+
+    return;
+}
---
+++ Codigo/Sorting/QuickSort.cpp
@@ -0,0 +1,44 @@
+// comparações: C(n) = O()
+// trocas: T(n) = O()
+// adptativo: O()
+
+// Vantagens:
+
+// Desvantagens:
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void troca(int *ar, int i, int j){
+    int aux;
+    aux = ar[i];
+    ar[i] = ar[j];
+    ar[j] = aux;
+    return;
+}
+
+int particao(int *ar, int l, int r){
+    int i = 0, fim = 0;
+    fim = l;
+    for(i = l + 1; i <= r; i++){
+        if(ar[i] < ar[l]){
+            fim = fim + 1;
+            troca(ar, fim, i);
+        }
+    }
+    troca(ar, l, fim);
+    return fim;
+}
+
+void quickSort(int *ar, int l, int r){
+    int i = 0;
+
+    if(l >= r) return;
+
+    i = particao(ar, l, r);
+    quickSort(ar, l, i - 1);
+    quickSort(ar, i + 1, r); 
+    
+    return;
+}
