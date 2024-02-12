88c6ce0fa43758da2e51355301ee291ecba05bd3
Gabriel Campelo <gabriel.campfor@gmail.com>
2024-02-12 13:22:30 -0300

    Rearrange perfect square codes

--- Codigo/Matematica/ceil_floor_perfectSquare.cpp
+++
@@ -1,8 +0,0 @@
-// Time Complexity: O(sqrt(n))
-// Auxiliary Space: O(1)
-#include <bits/stdc++.h>
-
-bool isPerfectSquare(long double n){
-    if(floor(sqrt(n)) == ceil(sqrt(n))) return true;
-    else return false;
-}
--- Codigo/Matematica/perfectSquare.cpp
+++ Codigo/Matematica/perfectSquare.cpp
@@ -1,7 +1,7 @@
-// Time Complexity: O(log n)
-// Auxiliary Space: O(1)
 #include <bits/stdc++.h>
 
+// Time Complexity: O(log n)
+// Auxiliary Space: O(1)
 bool isPerfectSquare(long double n){
     if(n >= 0){
         // se a raiz de n é inteira não haverá arredondamento para o menor inteiro
@@ -13,4 +13,11 @@ bool isPerfectSquare(long double n){
 
     //retorna falso caso x < 0
     return false;
+}
+
+// Time Complexity: O(sqrt(n))
+// Auxiliary Space: O(1)
+bool isPerfectSquare(long double n){
+    if(floor(sqrt(n)) == ceil(sqrt(n))) return true;
+    else return false;
 }
