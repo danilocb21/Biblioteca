ae341f7c22b0c3b1d63a83b9d0a2b849ad4fef50
Gabriel Campelo <gabriel.campfor@gmail.com>
2024-02-16 21:01:22 -0300

    Update sieve

--- Codigo/Matematica/sieve.cpp
+++ Codigo/Matematica/sieve.cpp
@@ -4,16 +4,16 @@ using namespace std;
 // Time Complexity: O(nloglogn)
 // Auxiliary Space: O(n)
 // Find primes in range [2, n]
-int main(){
-    int n;
-    cin >> n;
+vector<int> sieve(int n){
     vector<int> is_prime(n + 1, true);
     is_prime[0] = is_prime[1] = false;
-    for(int i = 2; i <= n; i++){
-        if(is_prime[i] && (long long)i * i <= n){
+
+    for(int i = 2; (long long)i * i <= n; i++){
+        if(is_prime[i]){
             for(int j = i * i; j <= n; j += i){
                 is_prime[j] = false;
             }
         }
     }
+    return is_prime;
 }
