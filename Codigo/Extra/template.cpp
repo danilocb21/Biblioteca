3385b31dc05283f50b8bd19ccae8e538e22a0c03
Gabriel Campelo Formiga <130695548+GabrielCFormiga@users.noreply.github.com>
2024-03-21 13:53:13 -0300

    Update template.cpp

--- Codigo/Extra/template.cpp
+++ Codigo/Extra/template.cpp
@@ -2,6 +2,7 @@
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
 #define pb push_back
+#define all(x) (x).begin(), (x).end()
 
 using namespace std;
 
@@ -9,11 +10,11 @@ typedef long long ll;
 typedef unsigned long long llu;
 
 int main(){ _
-    int tt, n;
+    int tt;
     cin >> tt;
-    while(tt--){
-        
-    }
+    while(tt--) {
 
+    }
+    
     return 0;
 }
