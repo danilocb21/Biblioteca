9f7bfa11be7ed4d990bd13c856aab71a6cd83367
Danilo <danilocbjp@hotmail.com>
2025-06-13 21:48:53 -0300

    update dijkstra and add various algorithms (graphs and strings)

--- Codigo/Grafos/dijkstra.cpp
+++ Codigo/Grafos/dijkstra.cpp
@@ -1,38 +1,35 @@
 // Dijkstra
 //
-// Econtra o menor caminho do vértice de index s até os outros vértices
+// Encontra o menor caminho do vértice de index s até os outros vértices
 //
-// O(n^2)
+// O(n log(n))
 
 const int INF = 0x3f3f3f3f;
 vector<vector<pair<int, int>>> adj; // {to, weight}
 
-int dijkstra(int s, vector<int> &dist, vector<int> &pred) {
-    int n = adj.size();
-    dist.assign(n, INF);
-    pred.assign(n, -1);
-    vector<bool> vis(n, false);
-    
+int dijkstra(int n, int s) {
+    vector<int> dist(n, INF);
+
+    // origem
     dist[s] = 0;
-    for (int i = 0; i < n; i++) {
-        int v = -1;
-        for (int j = 0; j < n; j++) {
-            // procura o nó não visitado de menor caminho
-            if (!vis[j] && (v == -1 || dist[j] < dist[v])) v = j;
-        }
-        
-        if (dist[v] == INF) break;
-        
-        vis[v] = true;
-        for (auto edge : adj[v]) {
-            int to = edge.first;
-            int len = edge.second;
-            
-            if (dist[v] + len < d[to]) {
-                d[to] = d[v] + len;
-                pred[to] = v;
+
+    using pi = pair<int,int>;
+    priority_queue<pi, vector<pi>, greater<pi>> q;
+    q.emplace(0,s);
+    while (!q.empty()) {
+        auto [w,u] = q.top();
+        q.pop();
+
+        if (u == n-1) break;
+        if (w != dist[u]) continue;
+
+        for (auto [W,v] : adj[u]) {
+            if (w+W < dist[v]) {
+                dist[v] = w+W;
+                q.emplace(w+W,v);
             }
         }
     }
-}
 
+    return dist[n-1];
+}
